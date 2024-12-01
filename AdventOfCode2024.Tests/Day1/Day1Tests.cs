0a90adc0dad1b9518c73aff3e36d6a3ce4302e0c
Jeffrey Boon <jeffrey.boon@pecunda.com>
2024-12-01 12:37:50 +0100

    Day1

---
+++ AdventOfCode2024.Tests/Day1/Day1Tests.cs
@@ -0,0 +1,34 @@
+using AdventOfCode2024.Day1;
+
+namespace AdventOfCode2024.Tests.Day1;
+
+public class Day1Tests
+{
+    [Theory]
+    [InlineData(@"Day1/example_input.txt", 11)]
+    [InlineData(@"Day1/input.txt", 1830467)]
+    public void Part1(string fileName, int expectedTotalDistance)
+    {
+        var lines = File.ReadLines(fileName).ToArray();
+        Assert.NotEmpty(lines);
+
+        var main = new Main(lines);
+        var actualTotalDistance = main.GetTotalDistance();
+
+        Assert.Equal(expectedTotalDistance, actualTotalDistance);
+    }
+
+    [Theory]
+    [InlineData(@"Day1/example_input.txt", 31)]
+    [InlineData(@"Day1/input.txt", 26674158)]
+    public void Part2(string fileName, int expectedSimilarityScore)
+    {
+        var lines = File.ReadLines(fileName).ToArray();
+        Assert.NotEmpty(lines);
+
+        var main = new Main(lines);
+        var actualSimilarityScore = main.GetSimilarityScore();
+
+        Assert.Equal(expectedSimilarityScore, actualSimilarityScore);
+    }
+}
---
+++ AdventOfCode2024/Day1/Main.cs
@@ -0,0 +1,63 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024.Day1;
+
+public partial class Main
+{
+    private readonly List<int> _leftList = [];
+    private readonly List<int> _rightList = [];
+
+    private int[] _orderedLeftList = null!;
+    private int[] _orderedRightList = null!;
+
+    public Main(string[] input)
+    {
+        ParseInput(input);
+    }
+
+    public int GetTotalDistance()
+    {
+        var totalDistance = 0;
+
+        var length = _orderedLeftList.Length;
+        for (var i = 0; i < length; i++)
+        {
+            totalDistance += Difference(_orderedLeftList[i], _orderedRightList[i]);
+        }
+
+        return totalDistance;
+    }
+
+    public int GetSimilarityScore()
+    {
+        var similarityScore = 0;
+
+        foreach (var number in _leftList)
+        {
+            var appearances = _rightList.Count(i => i == number);
+            similarityScore += number * appearances;
+        }
+
+        return similarityScore;
+    }
+
+    private void ParseInput(string[] input)
+    {
+        foreach (var line in input)
+        {
+            var splitLine = ThreeSpaces().Split(line);
+            _leftList.Add(int.Parse(splitLine[0]));
+            _rightList.Add(int.Parse(splitLine[1]));
+        }
+
+        _orderedLeftList = _leftList.Order().ToArray();
+        _orderedRightList = _rightList.Order().ToArray();
+    }
+
+    private static int Difference (int a, int b) {
+        return Math.Max(a,b) - Math.Min(a,b);
+    }
+
+    [GeneratedRegex(@"\s{3}")]
+    private static partial Regex ThreeSpaces();
+}
