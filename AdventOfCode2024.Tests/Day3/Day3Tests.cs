2958a9bf00caaee8cfb4d9d191479c1ee35c8abf
Jeffrey Boon <jeffrey.boon@pecunda.com>
2024-12-03 15:30:21 +0100

    Day3

---
+++ AdventOfCode2024.Tests/Day3/Day3Tests.cs
@@ -0,0 +1,36 @@
+using AdventOfCode2024.Day3;
+
+namespace AdventOfCode2024.Tests.Day3;
+
+public class Day3Tests
+{
+    [Theory]
+    [InlineData(@"Day3/example_input.txt", 161)]
+    [InlineData(@"Day3/input.txt", 175615763)]
+    public void Part1(string fileName, int correctAnswer)
+    {
+        var input = File.ReadAllText(fileName);
+        Assert.NotEmpty(input);
+
+        var main = new Main(input);
+
+        var actualAnswer = main.GetPart1Answer();
+
+        Assert.Equal(correctAnswer, actualAnswer);
+    }
+
+    [Theory]
+    [InlineData(@"Day3/example_input_2.txt", 48)]
+    [InlineData(@"Day3/input.txt", 74361272)]
+    public void Part2(string fileName, int correctAnswer)
+    {
+        var input = File.ReadAllText(fileName);
+        Assert.NotEmpty(input);
+
+        var main = new Main(input);
+
+        var actualAnswer = main.GetPart2Answer();
+
+        Assert.Equal(correctAnswer, actualAnswer);
+    }
+}
---
+++ AdventOfCode2024/Day3/Main.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024.Day3;
+
+public partial class Main(string input)
+{
+    public int GetPart1Answer()
+    {
+        var answer = 0;
+
+        var instructions = MulInstructions().Matches(input).Select(x => x.Value);
+
+        foreach (var instruction in instructions)
+        {
+            var numbers = instruction[4..^1].Split(',').Select(int.Parse).ToArray();
+            answer += numbers[0] * numbers[1];
+        }
+
+        return answer;
+    }
+
+    public int GetPart2Answer()
+    {
+        var answer = 0;
+        var executeInstruction = true;
+        var instructions = MulDoDontInstructions().Matches(input).Select(x => x.Value);
+
+        foreach (var instruction in instructions)
+        {
+            if (instruction == "do()")
+                executeInstruction = true;
+
+            else if (instruction == "don't()")
+                executeInstruction = false;
+
+            else if (executeInstruction)
+            {
+                var numbers = instruction[4..^1].Split(',').Select(int.Parse).ToArray();
+                answer += numbers[0] * numbers[1];
+            }
+        }
+
+        return answer;
+    }
+
+    [GeneratedRegex(@"mul\([0-9]{1,3},[0-9]{1,3}\)")]
+    private static partial Regex MulInstructions();
+
+    [GeneratedRegex(@"(mul\([0-9]{1,3},[0-9]{1,3}\))|(do\(\))|(don't\(\))")]
+    private static partial Regex MulDoDontInstructions();
+}
